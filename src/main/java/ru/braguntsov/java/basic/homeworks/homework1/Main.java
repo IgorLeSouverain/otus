79604e50f23d3e398daecb019cb7ba1f19ba5107
IgorLeSouverain <braguntsov10@gmail.com>
2024-01-17 09:02:07 +0300

    little fix

--- src/main/java/ru/braguntsov/java/basic/homeworks/homework1/Main.java
+++ src/main/java/ru/braguntsov/java/basic/homeworks/homework1/Main.java
@@ -10,14 +10,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int number = Integer.parseInt(scanner.nextLine());
 
-        switch (number)  {
+        switch (number) {
             case 1:
                 main.greetings();
                 break;
             case 2:
-                int var1 = (int)(Math.random()*50) - 30;
-                int var2 = (int)(Math.random()*50) - 30;
-                int var3 = (int)(Math.random()*50) - 30;
+                int var1 = (int) (Math.random() * 50) - 30;
+                int var2 = (int) (Math.random() * 50) - 30;
+                int var3 = (int) (Math.random() * 50) - 30;
                 System.out.println(main.checkSign(var1, var2, var3));
                 break;
             case 3:
@@ -27,14 +27,18 @@ public class Main {
                 main.selectColor();
                 break;
             case 5:
-                int var5 = (int)(Math.random()*30) - 30;
-                int var4 = (int)(Math.random()*30) - 30;
+                int var5 = (int) (Math.random() * 30) - 30;
+                int var4 = (int) (Math.random() * 30) - 30;
                 System.out.println(main.addOrSubtractAndPrint(var4, var5, false));
                 break;
         }
     }
+
     public void greetings() {
-        System.out.println("Hello World from Java");
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
     }
 
     public String checkSign(int a, int b, int c) {
