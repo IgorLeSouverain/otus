c3640ab6d4e133f00e9826d2a229fc63cb208393
IgorLeSouverain <braguntsov10@gmail.com>
2024-03-13 20:02:22 +0300

    init

---
+++ src/main/java/ru/braguntsov/java/basic/homeworks/homework6/Plate.java
@@ -0,0 +1,32 @@
+package ru.braguntsov.java.basic.homeworks.homework6;
+
+public class Plate {
+    private final int maxFood;
+    private int currentFood;
+
+    public Plate(int maxFood) {
+        this.maxFood = maxFood;
+        this.currentFood = maxFood;
+    }
+
+    public void addFood(int foodAmount) {
+        if (currentFood + foodAmount <= maxFood) {
+            currentFood += foodAmount;
+        } else {
+            System.out.println("В тарелке не может быть больше максимума еды");
+        }
+    }
+
+    public boolean decreaseFood(int foodAmount) {
+        if (currentFood - foodAmount >= 0) {
+            currentFood -= foodAmount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean hasFood() {
+        return currentFood > 0;
+    }
+}
