b2354cb9add4d2b33ff7a8a6aeeeb855a3c4e601
IgorLeSouverain <braguntsov10@gmail.com>
2024-03-14 11:03:19 +0300

    init

---
+++ src/main/java/ru/braguntsov/java/basic/homeworks/homework7/Human.java
@@ -0,0 +1,15 @@
+package ru.braguntsov.java.basic.homeworks.homework7;
+
+public class Human {
+    private final String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    
+
+    public String getName() {
+        return name;
+    }
+}
