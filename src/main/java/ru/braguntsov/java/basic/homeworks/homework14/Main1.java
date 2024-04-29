5e0967dc088ce863230c67b86b637aef2c3c3897
IgorLeSouverain <braguntsov10@gmail.com>
2024-04-29 15:33:55 +0300

    fixes

--- src/main/java/ru/braguntsov/java/basic/homeworks/homework14/Main1.java
+++ src/main/java/ru/braguntsov/java/basic/homeworks/homework14/Main1.java
@@ -1,18 +1,18 @@
 package ru.braguntsov.java.basic.homeworks.homework14;
 
 public class Main1 extends Thread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         createArray();
     }
 
-    public static void createArray() {
+    public static void createArray() throws InterruptedException {
         long start = System.nanoTime();
         int runs = 1000 * 1000;
         double[] array = new double[100_000_000];
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < array.length; i++) {
+                for (int i = 0; i < 25_000_000; i++) {
                     array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
                 }
             }
@@ -20,7 +20,7 @@ public class Main1 extends Thread {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < array.length; i++) {
+                for (int i = 25_000_000; i < 50_000_000; i++) {
                     array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
                 }
             }
@@ -28,7 +28,7 @@ public class Main1 extends Thread {
         Thread t3 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < array.length; i++) {
+                for (int i = 50_000_000; i < 75_000_000; i++) {
                     array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
                 }
             }
@@ -36,7 +36,7 @@ public class Main1 extends Thread {
         Thread t4 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < array.length; i++) {
+                for (int i = 75_000_000; i < 100_000_000; i++) {
                     array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
                 }
             }
@@ -45,6 +45,10 @@ public class Main1 extends Thread {
         t2.start();
         t3.start();
         t4.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
         long time = System.nanoTime() - start;
         System.out.printf("The average time taken was %.1f ns%n", (double) time / runs);
     }
